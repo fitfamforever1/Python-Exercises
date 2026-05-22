0e4341979158eed83972f4dd8774342917c6dbe5
ozneverdies <fitfamforever1@gmail.com>
2026-05-22 21:19:28 +0500

    Add dice rolling simulation with ASCII art
    This script simulates rolling a specified number of dice and displays the results in an artistic format. It also calculates and prints the total of the rolled dice.

---
+++ rice_roller.py
@@ -0,0 +1,51 @@
+import random
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+number_of_dice = int(input("How many dice do you want to roll? "))
+
+
+for x in range(0, number_of_dice):
+    dice.append(random.randint(1, 6))
+
+for y in range(0, 5):
+    for x in range(0, number_of_dice):
+        print(dice_art[dice[x]][y], end=" ")
+    print()
+
+print(f"Total: {sum(dice)}")
+
+
